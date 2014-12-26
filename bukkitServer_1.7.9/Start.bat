1fa7ca0ffe7318ca66ea2d59676e5196659ebfa9
danthemanzx <dan.the.man.zx@gmail.com>
2014-12-26 02:14:24 -0600

    Switched of to Spigot

--- bukkitServer_1.7.9/Start.bat
+++
@@ -1,5 +0,0 @@
-@Echo OFF
-Title Mc Bukkit Server
-Color F
-"%ProgramFiles%\Java\jre7\bin\java.exe" -jar craftbukkit-1.7.9-R0.2.jar
-pause
---
+++ spigotServer_1.7.9/Start.bat
@@ -0,0 +1,5 @@
+@Echo OFF
+Title Mc Bukkit Server
+Color F
+"%ProgramFiles%\Java\jre7\bin\java.exe" -jar spigot.jar
+pause
